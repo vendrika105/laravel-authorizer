8783fbba94a0e3380a1e6c0f4b83dc8ffc09dd25
vendrika105 <vendrika105@gmail.com>
2025-02-07 03:31:26 +0700

    Add interface for Authorizable Model

---
+++ src/interfaces/AuthorizableModel.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace Vendrika105\LaravelAuthorization\interfaces;
+
+interface AuthorizableModel
+{
+    public function getPermissions(): array;
+
+    public function hasPartialPermissions(...$permissions): bool;
+
+    public function hasExactPermissions(...$permissions): bool;
+
+    public function hasPermission(...$permissions): bool;
+
+    public function can(...$permissions): bool;
+
+    public function assignPermission(...$permissions): bool;
+
+    public function removePermission(...$permissions): bool;
+
+
+    public function getRoles(): array;
+
+    public function hasPartialRoles(...$roles): bool;
+
+    public function hasExactRoles(...$roles): bool;
+
+    public function hasRoles(...$roles): bool;
+
+    public function is(...$roles): bool;
+
+    public function assignRoles(...$roles): bool;
+
+    public function removeRoles(...$roles): bool;
+}
