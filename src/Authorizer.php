7f0b306101f68b5aff5283fffc6e9bb30c13647c
vendrika105 <vendrika105@gmail.com>
2025-02-07 03:12:30 +0700

    Add empty method for role and permission getter and checker

--- src/Authorizer.php
+++ src/Authorizer.php
@@ -4,5 +4,33 @@ namespace Vendrika105\LaravelAuthorization;
 
 class Authorizer
 {
+    public function hasExactPermissions(...$permissions): bool
+    {
 
+    }
+
+    public function hasPartialPermissions(...$permissions): bool
+    {
+
+    }
+
+    public function hasExactRoles(...$roles): bool
+    {
+
+    }
+
+    public function hasPartialRoles(...$roles): bool
+    {
+
+    }
+
+    public function getRoles(): array
+    {
+
+    }
+
+    public function getPermissions(): array
+    {
+        
+    }
 }
