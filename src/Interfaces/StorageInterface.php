63c530993c265255e4402632f5e9daaaf88dfeb1
vendrika105 <vendrika105@gmail.com>
2025-02-09 07:05:08 +0700

    Add signature for fetching user data and update user data

--- src/Interfaces/StorageInterface.php
+++ src/Interfaces/StorageInterface.php
@@ -4,5 +4,7 @@ namespace Vendrika105\LaravelAuthorization\Interfaces;
 
 interface StorageInterface
 {
+    public function fetchUserData(string|int $user_id): array;
 
+    public function updateUserData(string|int $user_id, array $data);
 }
