ef17939c559897c2b697b4cfd5bb21409a356903
vendrika105 <vendrika105@gmail.com>
2025-02-10 07:41:17 +0700

    Add empty method

--- src/Storage/DatabaseStorage.php
+++ src/Storage/DatabaseStorage.php
@@ -2,7 +2,17 @@
 
 namespace Vendrika105\LaravelAuthorization\Storage;
 
-class DatabaseStorage
-{
+use Vendrika105\LaravelAuthorization\Interfaces\StorageInterface;
 
+class DatabaseStorage implements StorageInterface
+{
+    public function fetchUserData(int|string $user_id): array
+    {
+        // TODO: Implement fetchUserData() method.
+    }
+
+    public function updateUserData(int|string $user_id, array $data)
+    {
+        // TODO: Implement updateUserData() method.
+    }
 }
