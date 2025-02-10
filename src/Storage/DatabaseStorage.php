5693bd96c21a5ee55de3bee9dd3d5b6c4a609077
vendrika105 <vendrika105@gmail.com>
2025-02-10 07:52:55 +0700

    Add empty method for user permission, and role permission fetching

--- src/Storage/DatabaseStorage.php
+++ src/Storage/DatabaseStorage.php
@@ -2,6 +2,8 @@
 
 namespace Vendrika105\LaravelAuthorization\Storage;
 
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Storage;
 use Vendrika105\LaravelAuthorization\Interfaces\StorageInterface;
 
 class DatabaseStorage implements StorageInterface
@@ -9,6 +11,26 @@ class DatabaseStorage implements StorageInterface
     public function fetchUserData(int|string $user_id): array
     {
         // TODO: Implement fetchUserData() method.
+    }
+
+    protected function fetchUserPermissions(int|string $user_id): array
+    {
+
+    }
+
+    protected function fetchUserRoles(int|string $user_id): array
+    {
+
+    }
+
+    protected function fetchRoleChildPermissions(int|string $role_id): array
+    {
+
+    }
+
+    protected function fetchRolePermissions(array $roles): array
+    {
+
     }
 
     public function updateUserData(int|string $user_id, array $data)
